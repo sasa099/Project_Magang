9faa75e0f5dd0a2b18464422bf4f29e3c3b1e7dd
Anastasia <anastasiaaprilia099@gmail.com>
2021-11-10 14:16:10 +0700

    first commit

--- app/models/datamhs.model.js
+++ app/models/datamhs.model.js
@@ -2,17 +2,26 @@ module.exports = (mongoose) => {
     const Datamhs = mongoose.model(
       "datamhs",mongoose.Schema({
         nim:String,
+        nik:String,
         nama_depan:String,
         nama_belakang:String,
+        jenis_kelamin:String,
         prodi:String,
         email:String,
         alamat:String,
         notelp:String,
         alamatortu:String,
+        kodepos:String,
+        kodeposs:String,
+        provinsimu:String,
+        provinsi:String,
+        kotamu:String,
+        kota:String,
+        kecamatanmu:String,
         foto:String,
         nik:String,
-        gender:String, 
-        kelas: String,
+        gender:String,
+        kelas:String,
       }, {
         timestamps: true
       })
